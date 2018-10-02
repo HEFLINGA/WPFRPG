f97069729561f1ea3ac4035e4263e76fb61eb33b
Auston Hefling <auston.hefling@outlook.com>
2018-10-02 12:34:07 -0400

    Add locations in World Factory

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+
+            newWorld.AddLocation(0, -1, "Home"
+                , "This is your Home"
+                , "/Engine;component/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1, -1, "Farmer's House"
+                , "This is the house of your neighbor, Farmer Ted."
+                , "/Enging;component/Images/Locations/FarmHouse.png");
+
+            newWorld.AddLocation(-2, -1, "Farmer's Field"
+                , "There are rows of corn frowing here, with giant rats hiding between them."
+                , "/Enging;component/Images/Locations/FarmFields.png");
+
+            return newWorld;
+        }
+    }
+}
---
+++ Engine/Models/World.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        // Empty list that holds our Location objs
+        private List<Location> _locations = new List<Location>();
+
+        // Default Constructor
+        public World() { }
+
+        internal void AddLocation(int xCoord, int yCoord, string name, string desc, string imageName)
+        {
+            Location loc = new Location
+            {
+                XCoordinate = xCoord,
+                YCoordinate = yCoord,
+                Name = name,
+                Description = desc,
+                ImageName = imageName
+            };
+
+            _locations.Add(loc);
+        }
+
+        public Location LocationAt(int xCoord, int yCoord)
+        {
+            foreach (Location loc in _locations)
+            {
+                if (loc.XCoordinate == xCoord && loc.YCoordinate == yCoord)
+                {
+                    return loc;
+                }
+            }
+
+            return null;
+        }
+    }
+}
